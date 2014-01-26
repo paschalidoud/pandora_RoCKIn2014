5afff6ae014cc75c7c361e33beecde06860b18c9
passchalidoud <paschalidoud@gmail.com>
2014-01-26 10:32:04 +0200

    pandora_RoCKIn initialized working repo

---
+++ devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/paschalidoud/pandora_RoCKIn2014/build/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
