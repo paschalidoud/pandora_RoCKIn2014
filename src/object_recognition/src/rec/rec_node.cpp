cb9b6b1319f542b19912112470686292d05afe5f
paschalidoud <paschalidoud@gmail.com>
2014-01-30 11:23:14 +0100

    rec_node working with asus primesence

--- src/object_recognition/src/rec/rec_node.cpp
+++ src/object_recognition/src/rec/rec_node.cpp
@@ -14,6 +14,7 @@
 #include "v4r/Tracker/Tracker.h"
 #include "rec/recognizer.h"
 #include <ros/package.h>
+#include <string>
 class RecNode
 {
   private:
@@ -23,16 +24,17 @@ class RecNode
 
     Recognizer *m_recognizer;
     TomGine::tgModel m_model;
-
+	
+	
     bool recognize(rec_msg_and_serv::rec::Request & req,
                    rec_msg_and_serv::rec::Response & response)
     {
 
 	   ROS_ERROR("start recognition ..\n");
-       for(size_t i = 0; i < models_.size(); i++)
-       {
-        ROS_ERROR("checking model %s \n", models_[i].c_str() );
-        loadModel(models_[i]);
+	   if(object_flag==1)
+	   {
+		ROS_ERROR("checking model %s \n", models_[1].c_str() );
+        loadModel(models_[1]);
         cv_bridge::CvImagePtr cv_image = cv_bridge::toCvCopy(req.image, "bgr8");;
         cv::imshow("to recognize", cv_image->image);
         cv::waitKey(10);
@@ -41,8 +43,8 @@ class RecNode
         recognizeModel(cv_image->image, pose, confidence);
         if(confidence > 0.)
         {
-          ROS_ERROR("recognized %s with confidence %s \n", models_[i].c_str(), confidence );
-          std::string name = models_[i].substr(models_[i].find_last_of('/'), models_[i].npos);
+          ROS_ERROR("recognized %s with confidence %s \n", models_[1].c_str(), confidence );
+          std::string name = models_[1].substr(models_[1].find_last_of('/'), models_[1].npos);
           std_msgs::String ss;
           ss.data = name;
           geometry_msgs::Pose gPose;
@@ -56,9 +58,39 @@ class RecNode
           response.models_found.push_back(ss);
           response.confidences_found.push_back(confidence);
           response.poses_found.push_back(gPose);
-        }
-      }
-
+        } 
+        object_flag=0;
+	   }
+       else if(object_flag==2)
+	   {
+		   ROS_ERROR("checking model %s \n", models_[2].c_str() );
+        loadModel(models_[2]);
+        cv_bridge::CvImagePtr cv_image = cv_bridge::toCvCopy(req.image, "bgr8");;
+        cv::imshow("to recognize", cv_image->image);
+        cv::waitKey(10);
+        TomGine::tgPose pose;
+        float confidence = 0.;
+        recognizeModel(cv_image->image, pose, confidence);
+        if(confidence > 0.)
+        {
+          ROS_ERROR("recognized %s with confidence %s \n", models_[2].c_str(), confidence );
+          std::string name = models_[2].substr(models_[2].find_last_of('/'), models_[2].npos);
+          std_msgs::String ss;
+          ss.data = name;
+          geometry_msgs::Pose gPose;
+          gPose.position.x = pose.t.x;
+          gPose.position.y = pose.t.y;
+          gPose.position.z = pose.t.z;
+          gPose.orientation.x = pose.q.x;
+          gPose.orientation.y = pose.q.y;
+          gPose.orientation.z = pose.q.z;
+          gPose.orientation.w = pose.q.w;
+          response.models_found.push_back(ss);
+          response.confidences_found.push_back(confidence);
+          response.poses_found.push_back(gPose);
+        } 
+        object_flag=0;
+	   }
       ROS_ERROR( ".. done recognition.\n");
 
       return true;
@@ -67,6 +99,7 @@ class RecNode
     bool check_shader_path();
     void loadModel(const std::string &modelname);
     void recognizeModel(const cv::Mat& img, TomGine::tgPose& pose, float &confidence);
+    void chatterCallback(const std_msgs::String::ConstPtr& msg);
 
   public:
     RecNode()
@@ -82,12 +115,12 @@ class RecNode
       delete m_recognizer;
     }
 
-    void initialize(int num, std::string path)
+    void initialize(int num, std::vector<std::string> path)
     {
       for(int i = 0; i < num; i++)
       {
-        ROS_ERROR("model: %s \n", path.c_str()); 
-        models_.push_back(path.c_str());
+        ROS_ERROR("model: %s \n", path.at(i).c_str()); 
+        models_.push_back(path.at(i).c_str());
       }
 
       if(models_.empty())
@@ -100,8 +133,10 @@ class RecNode
 
     void run()
     {
+	  ros::Subscriber sub = n_.subscribe("/nlu/object", 1000, &RecNode::chatterCallback,this);
       ros::spin();
     }
+    int object_flag;
 };
 
 bool RecNode::check_shader_path()
@@ -123,7 +158,19 @@ bool RecNode::check_shader_path()
 
   return true;
 }
-
+void RecNode::chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+   ROS_ERROR("I heard: [%s]", msg->data.c_str());
+   if(strcmp(msg->data.c_str(),"cereal")==0)
+   {
+	 object_flag=1;  
+   }
+   else if(strcmp(msg->data.c_str(),"coke")==0)
+   {
+	object_flag=2;
+   }
+   
+}
 void RecNode::loadModel(const std::string &filename)
 {
   m_recognizer->load(filename);
@@ -139,10 +186,18 @@ int main (int argc, char ** argv)
   ros::init (argc, argv, "rec");
 
   RecNode r;
-  //~ std::string path = ros::package::getPath("object_recognition");
-  std::string model_path= "/home/paschalidoud/pandora_RoCKIn2014/src/object_recognition/models";
-  //~ path.append(model_path);
-  r.initialize (1, model_path);
+  std::vector<std::string> models;
+  //~ std::string param_path = ros::package::getPath("object_recognition");
+  //~ std::string temp1;
+  //~ std::string temp2;
+  std::string model1_path= "/home/paschalidoud/pandora_RoCKIn2014/src/object_recognition/models/new_musli_351.sift";
+  std::string model2_path= "/home/paschalidoud/pandora_RoCKIn2014/src/object_recognition/models/can2_149f.sift";
+  //~ temp1.append(model1_path);
+  //~ temp2.append(model2_path);
+  models.push_back(model1_path);
+  models.push_back(model2_path);
+  r.object_flag=0;
+  r.initialize (2, models); 
   r.run();
 
   return 0;
--- src/object_recognition/src/rec_test/main.cpp
+++ src/object_recognition/src/rec_test/main.cpp
@@ -9,6 +9,7 @@
 #include "sensor_msgs/Image.h"
 #include "std_msgs/String.h"
 #include "rec_msg_and_serv/rec.h"
+#include <tf/transform_broadcaster.h>
 
 class RecDemo
 {
@@ -18,7 +19,9 @@ class RecDemo
     ros::NodeHandle n_;
     std::string camera_topic_;
     bool KINECT_OK_;
-
+    tf::Transform transform_auxiliar;
+	tf::TransformBroadcaster tf_br;
+	
     void
     checkCloudArrive (const sensor_msgs::Image::ConstPtr& msg)
     {
@@ -61,12 +64,19 @@ class RecDemo
         if (client.call(srv))
         {
          ROS_ERROR("Found models: %d ", static_cast<int>(srv.response.models_found.size()));
+		 
           for(size_t i=0; i < srv.response.models_found.size(); i++)
           {
             std::cout << "   => " << srv.response.models_found[i] << "(" << srv.response.confidences_found[i] << ") ";
             std::cout << " [" << srv.response.poses_found[i].position.x <<
                          ", " << srv.response.poses_found[i].position.y <<
                          ", " << srv.response.poses_found[i].position.z << "]\n";
+            transform_auxiliar.setOrigin(tf::Vector3(srv.response.poses_found[i].position.x, srv.response.poses_found[i].position.y,
+            srv.response.poses_found[i].position.y));
+			transform_auxiliar.setRotation(tf::Quaternion( srv.response.poses_found[i].position.x, srv.response.poses_found[i].orientation.y, 
+			srv.response.poses_found[i].orientation.z, srv.response.poses_found[i].orientation.w));
+
+			tf_br.sendTransform(tf::StampedTransform(transform_auxiliar, ros::Time::now(), "openni_camera", "/target_object"));
           }
         }
         else
@@ -81,7 +91,8 @@ class RecDemo
     RecDemo()
     {
       KINECT_OK_ = false;
-      camera_topic_ = "/camera_head/image_raw";
+      camera_topic_ = "/camera/rgb/image_raw";
+      //~ camera_topic_ ="camera_head/image_raw";
       kinect_trials_ = 5;
     }
 
